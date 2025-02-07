52977107b336210a15299655b3efce124ccbe5a0
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-07 23:03:00 +0300

    React: Задание №10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 function App() {
-	return(
-		<ul>
-			<li>text1</li>
-			<li>text2</li>
-			<li>text3</li>
-		</ul>);
+	return <><ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul></>;
+// или вариант с общим div
+
+	// <div>
+	// 	<div>
+	// 		<p>text1</p>
+	// 		<p>text2</p>
+	// 	</div>
+	// 	<div>
+	// 		<p>text3</p>
+	// 		<p>text4</p>
+	// 	</div>
+	// </div>
+
 }
 
 export default App;
