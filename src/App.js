4dde7cc1e425fbc13f34e66a80a88294a599b31a
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 00:18:44 +0300

    React: Задание №24

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
 
 function App() {
-	return <>
-	<ul>
-		<li>1</li>
-		<li>2</li>
-		<li>3</li>
-		<li>4</li>
-		<li>5</li>
-		<li>6</li>
-		<li>7</li>
-		<li>8</li>
-		<li>9</li>
-		<li>10</li>
-	</ul>
-	<div>
-	<table>
-		<tbody>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-			</tr>
-			<tr>
-				<td>4</td>
-				<td>5</td>
-				<td>6</td>
-			</tr>
-			<tr>
-				<td>7</td>
-				<td>8</td>
-				<td>9</td>
-			</tr>
-		</tbody>
-	</table>
-	</div>
-	</>
+	const num1 = 3;
+	const num2 = 2;
+	const num = 4;
+	const name = 'john';
+	const surname = 'smit';
+	
+	return <div>
+	Задание 1 -	result: {num1 ** num2} // num1 в степени num2 <br />
+	Задание 2 -	result: {name + ' ' + surname} // john"пробел"smit <br />
+	Задание 3 -	result: {Math.sqrt(num)} // корень из num
+	</div>;
 }
 
 export default App;
