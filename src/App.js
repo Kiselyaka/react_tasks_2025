4ab72be3f7bf194a0daabb1825aa9698ee7e1bc7
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:24:43 +0300

    React: Задание №38

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 function App() {
-	const arr = [
-		<li>1</li>,
-		<li>2</li>,
-		<li>3</li>,
-		<li>4</li>,
-		<li>5</li>,
-	];
-
+	const arr = [];
+	
+	for (let i = 1; i <= 5; i++) {
+		arr.push(<li>{i}</li>);
+	}
+	
 	return <div>
+		<ul>
 		{arr}
-	</div>
+		</ul>
+	</div>;
+	// вся равно копируешь, да -_-
 }
 
 export default App;
