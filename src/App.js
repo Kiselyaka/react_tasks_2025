d421f2ac965eab41cf4af4e7422fe3ce4c1dd84b
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:44:13 +0300

    React: Задание №42

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function App() {
 	const users = [
-		{name: 'user1', surn: 'surn1', age: 30},
-		{name: 'user2', surn: 'surn2', age: 31},
-		{name: 'user3', surn: 'surn3', age: 32},
+		{id: 1, name: 'user1', surn: 'surn1', age: 30},
+		{id: 2, name: 'user2', surn: 'surn2', age: 31},
+		{id: 3, name: 'user3', surn: 'surn3', age: 32},
 	];
 
-	const res = users.map(function(item, index) {
-		return <p key={index}>
+	const res = users.map(function(item) {
+		return <p key={item.id}>
 			<li>Имя пользователя: {item.name}, фамилия пользователя: {item.surn}, возраст пользователя: {item.age}</li>
 		</p>;
 	});
