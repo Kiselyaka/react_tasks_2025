8d881b52605d94525288d7303a13948bf8f46a86
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 00:27:51 +0300

    React: Задание №26

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 function App() {
-	const num1 = 3;
-	const num2 = 2;
-	const num = 4;
-	const name = 'john';
-	const surname = 'smit';
+	let text;
+	const isAdult = true;
+	
+	if (isAdult) {
+		text = <p>text true</p>;
+	}
+	else{
+		text = <p>text false</p>
+	}
 	
 	return <div>
-	Задание 1 -	result: {num1 ** num2} // num1 в степени num2 <br />
-	Задание 2 -	result: {name + ' ' + surname} // john"пробел"smit <br />
-	Задание 3 -	result: {Math.sqrt(num)} // корень из num
+		{text}
 	</div>;
 }
 
