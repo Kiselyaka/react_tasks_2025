e995cb03b0ea271b79e9d3fb2e9e1d82720920a9
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:17:41 +0300

    React: Задание №30

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
 	const isAuth = false;
 	
 	return <div>
-		{!isAuth && <p>пожалуйста, авторизуйтесь</p>}
+		{!isAuth && <p>пожалуйста, авторизуйтесь. Код не копируй -_-</p>}
 	</div>;
 }
 
