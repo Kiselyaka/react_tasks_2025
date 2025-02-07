7c384f1541f67569d86d2e09873508051c3fcfa4
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:10:19 +0300

    React: Задание №35

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 function App() {
-	function show(num) {
-		alert("Выводим " + num);
-	}
-	
-	return <div>
-		<button onClick={() => show(1)}>act1</button> <br />
-		<button onClick={() => show(2)}>act2</button> <br />
-		<button onClick={() => show(3)}>act2</button>
-	</div>;
+  function handleClick1(event) {
+    console.log(event); 
+  }
+
+  function handleClick2(event) {
+    console.log(event.target); 
+  }
+  
+  return (
+    <div>
+      <h2>Задание 1:</h2>
+      <button onClick={handleClick1}>act</button>
+      
+      <h2>Задание 2:</h2>
+      <button onClick={handleClick2}>act</button>
+    </div>
+  );
 }
 
 export default App;
