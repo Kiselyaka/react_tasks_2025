c03ab817e33e8cf7b5fec3cafc4a3972471b145a
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:16:47 +0300

    React: Задание №36

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
 function App() {
-  function handleClick1(event) {
-    console.log(event); 
+  function func1(arg, event) {
+    console.log('Задание 1:', arg, event);
   }
 
-  function handleClick2(event) {
-    console.log(event.target); 
+  function func2(arg1, arg2, event) {
+    console.log('Задание 2:', arg1, arg2, event);
+  }
+
+  function func3(event, arg) {
+    console.log('Задание 3:', event, arg);
+  }
+
+  function func4(arg1, event, arg2) {
+    console.log('Задание 4:', arg1, event, arg2);
   }
   
   return (
     <div>
       <h2>Задание 1:</h2>
-      <button onClick={handleClick1}>act</button>
-      
+      <button onClick={event => func1('eee', event)}>act</button>
+
       <h2>Задание 2:</h2>
-      <button onClick={handleClick2}>act</button>
+      <button onClick={event => func2('eee', 'а дима код копирует', event)}>act</button>
+
+      <h2>Задание 3:</h2>
+      <button onClick={event => func3(event, 'eee')}>act</button>
+
+      <h2>Задание 4:</h2>
+      <button onClick={event => func4('arg1', event, 'arg2')}>act</button>
     </div>
   );
 }
