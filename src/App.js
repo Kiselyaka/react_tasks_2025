dbfca188e2f1ec290e127927b49bca5195e4eee6
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 01:41:26 +0300

    React: Задание №41

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
-	
-	const res = arr.map(function(item, index) {
-		return <li key={index}>{item}</li>;
+	const users = [
+		{name: 'user1', surn: 'surn1', age: 30},
+		{name: 'user2', surn: 'surn2', age: 31},
+		{name: 'user3', surn: 'surn3', age: 32},
+	];
+
+	const res = users.map(function(item, index) {
+		return <p key={index}>
+			<li>Имя пользователя: {item.name}, фамилия пользователя: {item.surn}, возраст пользователя: {item.age}</li>
+		</p>;
 	});
-	
+	// совесть не мучает?)
 	return <div>
 		<ul>
 		{res}
