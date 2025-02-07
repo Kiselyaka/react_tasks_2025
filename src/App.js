03840d0db5c56f7781ff2b582cd131052d7b1572
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 02:13:16 +0300

    React: Задание №49

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ function id() {
 
 function App() 
 {
-  const uniqueId = id();
+	const prods = [
+		{id: id(), name: 'product1', cost: 100},
+		{id: id(), name: 'product2', cost: 200},
+		{id: id(), name: 'product3', cost: 300},
+	];
+	
+	const res = prods.map(function(item) {
+			return <p key={item.id}>{item.name} {item.cost}</p>;
+	});	
 
   return (
     <div>
-      <h1>Сгенерированный ID: {uniqueId}</h1>
+      {res}
     </div>
   );
 }
