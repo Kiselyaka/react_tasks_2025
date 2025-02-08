597bf96f4b2c01ed455bbd6f8bc27256d775d60e
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-09 01:20:38 +0300

    React: Задание №57

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import { useState } from 'react';
 // import { v4 as uuidv4 } from 'react-uuid'; // Для второго варианта
 
 function App() {
-	const [value, setValue] = useState('text');
-	const [value2, setValue2] = useState('text');
-	
-	function handleChange(event) {
-		setValue(event.target.value); 
-	}
+	const [value, setValue] = useState('');
 
-	function handleChange2(event) {
-		setValue2(event.target.value); 
+	function handleChange(event) {
+		setValue(event.target.value);
 	}
 	
-	return <div>
-		<input value={value} onChange={handleChange} />
-		<p>text: {value}</p>
-		<input value={value2} onChange={handleChange2} />
-		<p>text: {value2}</p>
-	</div>;
+	return (
+		<div>
+			<input value={value} onChange={handleChange} />
+			<p>Количество символов: {value.length}</p>
+		</div>
+	);
 }
 
 export default App;
