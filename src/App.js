7870f169b34622bc0312d2ebb2fd3a1b12c7524f
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-09 01:18:54 +0300

    React: Задание №56

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import { useState } from 'react';
 // import { v4 as uuidv4 } from 'react-uuid'; // Для второго варианта
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [value, setValue] = useState('text');
+	const [value2, setValue2] = useState('text');
 	
-	function clickLow() {
-		setCount(count - 1);
+	function handleChange(event) {
+		setValue(event.target.value); 
 	}
-	function clickUp() {
-		setCount(count + 1);
+
+	function handleChange2(event) {
+		setValue2(event.target.value); 
 	}
-	
 	
 	return <div>
-		<span>{count}</span>
-		<button onClick={clickUp}>увеличить значение</button>
-		<button onClick={clickLow}>уменьшить значение</button>
+		<input value={value} onChange={handleChange} />
+		<p>text: {value}</p>
+		<input value={value2} onChange={handleChange2} />
+		<p>text: {value2}</p>
 	</div>;
 }
 
