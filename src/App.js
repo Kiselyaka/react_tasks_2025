669a4a68b99d98e5ab8b591f044458d2ac546e82
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-09 00:49:21 +0300

    React: Задание №52

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { nanoid } from 'nanoid'; 
+import { useState } from 'react';
+
 // import { v4 as uuidv4 } from 'react-uuid'; // Для второго варианта
-	
-function id() {
-  return nanoid(); 
-}
 
 function App() 
 {
-	const prods = [
-		{id: id(), name: 'product1', cost: 100},
-		{id: id(), name: 'product2', cost: 200},
-		{id: id(), name: 'product3', cost: 300},
-	];
-	
-	const res = prods.map(function(item) {
-			return <p key={item.id}>{item.name} {item.cost}</p>;
-	});	
-
+	const [name_user, setName1] = useState('Maxim');
+	const [name_last, setName2] = useState('Kiseliov');
+	const [age, setName3] = useState('18');
+	const changeName = () => {
+		setName1('Новое имя'); // Вы можете изменить на нужное вам значение
+	  };
   return (
     <div>
-      {res}
+      Имя пользователя: {name_user} {name_last}, его возраст: {age} <br />
+	  <button onClick={changeName}>Изменить имя</button>
     </div>
   );
 }
