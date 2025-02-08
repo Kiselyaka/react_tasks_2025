c6e7ef1c5813387a6ed5d99e4ec3012821428461
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-09 01:26:50 +0300

    React: Задание №58

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import { useState } from 'react';
 // import { v4 as uuidv4 } from 'react-uuid'; // Для второго варианта
 
 function App() {
-	const [value, setValue] = useState('');
+    const [age, setAge] = useState(0);
+    const currentYear = new Date().getFullYear();
+    
+    function handleChange(event) {
+        setAge(event.target.value);
+    }
+    
+	const [fahrenheit, setFahrenheit] = useState(0);
 
-	function handleChange(event) {
-		setValue(event.target.value);
-	}
-	
-	return (
+    function handleChange2(event) {
+        setFahrenheit(event.target.value);
+    }
+
+    const birthYear = currentYear - age;
+	const celsius = (fahrenheit - 32) * 5/9;
+
+    return (
+		<>
+        <div>
+            <input type="number" value={age} onChange={handleChange} placeholder="Введите ваш возраст"/>
+            <p>Ваш год рождения: {birthYear}</p>
+        </div>
 		<div>
-			<input value={value} onChange={handleChange} />
-			<p>Количество символов: {value.length}</p>
-		</div>
-	);
+		<input 
+			type="number" value={fahrenheit} onChange={handleChange2} placeholder="Введите градусы Фаренгейта"/>
+		<p>Градусы Цельсия: {celsius.toFixed(2)}</p>
+	    </div>
+		</>
+    );
 }
 
 export default App;
