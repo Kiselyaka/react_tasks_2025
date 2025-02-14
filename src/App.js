7dc42db071334b19d39cdd833493fc90e5cf1757
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-14 22:37:28 +0300

    React: Задание №65

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 
 function App() {
+	const texts = ['Ялта', 'Симферополь', 'Москва', 'Санкт-Петербург'];
 	const [value, setValue] = useState('');
 	
-	function handleChange(event) {
-		setValue(event.target.value);
-	}
+	const options = texts.map((text, index) => {
+	return <option key={index}>{text}</option>;
+	});
 	
 	return <div>
-		<select value={value} onChange={handleChange}>
-			<option>Симферополь</option>
-			<option>Ялта</option>
-			<option>Москва</option>
-			<option>Санкт-Петербург</option>
+		<select value={value} onChange={(event) => setValue(event.target.value)}>
+			{options}
 		</select>
 		<p>
 			ваш выбор: {value}
