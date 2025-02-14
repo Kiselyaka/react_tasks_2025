90f7f955c56d773b71bc97e3354bf3133c7375b5
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-14 22:50:31 +0300

    React: Задание №66

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 function App() {
-	const texts = ['Ялта', 'Симферополь', 'Москва', 'Санкт-Петербург'];
 	const [value, setValue] = useState('');
 	
-	const options = texts.map((text, index) => {
-	return <option key={index}>{text}</option>;
-	});
-	
 	return <div>
-		<select value={value} onChange={(event) => setValue(event.target.value)}>
-			{options}
+		<select value={value} onChange={event => setValue(event.target.value)}>
+			<option value="1">мне от 0 до 12 лет</option>
+			<option value="2">мне от от 13 до 17 лет</option>
+			<option value="3">мне от от 18 до 25 лет</option>
+      <option value="4">я больше 25 лет</option>
 		</select>
 		<p>
-			ваш выбор: {value}
+			{value === '1' && 'вы выбрали первый пункт -  от 0 до 12 лет'}
+			{value === '2' && 'вы выбрали второй пункт - от 13 до 17 лет'}
+			{value === '3' && 'вы выбрали третий пункт - от 18 до 25 лет'}
+      {value === '4' && 'вы выбрали третий пункт - старше 25 лет'}
 		</p>
 	</div>;
 }
