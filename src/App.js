7ee82e8440b1bb655557e074224184652f4929a1
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-14 22:10:59 +0300

    React: Задание №62

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import React, { useState } from 'react';
 
-function translit(text) {
-  const translitMap = {
-    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 
-    'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 
-    'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 
-    'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 
-    'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 
-    'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 
-    'ю': 'yu', 'я': 'ya',
-  };
-
-  // или через цикл и кейсы => прибавлять символы к строке, но мне лень
-
-  return text.split('').map(char => translitMap[char] || char).join('');
-}
-
 function App() {
-  const [value, setValue] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [message, setMessage] = useState(''); 
 
-  function handleChange(event) {
-    setValue(event.target.value);
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
+
+  function show() {
+    if (checked) {
+      setMessage('Привет, старый друг'); 
+    } else {
+      setMessage('Вы не нажали на чекбокс');
+    }
   }
-
-  const [value2, setValue2] = useState('');
-
-  function handleChange2(event) {
-    setValue2(event.target.value);
-  }
-
-  const sum = value2
-  .split('\n')
-  .map(line => parseFloat(line)) 
-  .filter(num => !isNaN(num)) 
-  .reduce((acc, curr) => acc + curr, 0); 
 
   return (
     <div>
-      <div> 
-      Задание 1:
-      <textarea value={value} onChange={handleChange} />
-      <p>{translit(value)}</p>
-      </div>
       <div>
-      Задание 2:
-      <textarea value={value2} onChange={handleChange2} />
-      <p>Сумма: {sum}</p>
-      </div>
+        Задание 1:
+      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
+      <p>состояние: {checked ? 'отмечен' : 'не отмечен'}</p>
+      <button onClick={show}>Показать сообщение</button>
+      <p>{message}</p> 
+    </div>
+    <div>
+      Задание 2: <br />
+      Выберите языки программирования, которые вы знаете: <br />
+      Если вы знаете html, проставте галочку:
+      <input type="checkbox" checked={checked1} onChange={() => setChecked1(!checked1)} />
+      {checked1 ? <p>О, супер. Вы знаете html, жаль что это не язык программирования</p> : <p>не отмечен</p>}
+
+       Если вы знаете js, проставте галочку:
+      <input type="checkbox" checked={checked2} onChange={() => setChecked2(!checked2)} />
+       {checked2 ? <p>О, супер. Вы знаете js</p> : <p>не отмечен</p>}
+
+      Если вы знаете css, проставте галочку:
+      <input type="checkbox" checked={checked3} onChange={() => setChecked3(!checked3)} />
+      {checked3 ? <p>О, супер. Вы знаете css, жаль что это не язык программирования</p> : <p>не отмечен</p>}
+    </div>
     </div>
   );
 }
