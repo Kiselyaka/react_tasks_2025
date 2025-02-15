864bda33fec1cbf0964dc267b7a10b9a14475c50
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-15 21:16:05 +0300

    React: Задание №74

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { useState } from 'react';
 
 function App() {
-	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+  const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
   const [inputValue, setInputValue] = useState('');
-	
-  const addNote = () => {
-    setNotes([...notes, notes.length + 1]);
-  };
 
-  const addNote2 = () => {
-    if (inputValue.trim() !== '') { 
-      setNotes([...notes, inputValue]); 
-      setInputValue(''); 
+  function doSmth(index) {
+    let copy = [...notes];
+    copy[index] = copy[index] ** 2; 
+    setNotes(copy);
+  }
+
+  // Удаление элемента
+  function removeNote(index) {
+    let copy = notes.filter((_, i) => i !== index); 
+    setNotes(copy);
+  }
+
+  // Ввод текста в инпут при клике на li
+  const result = notes.map((note, index) => {
+    return (
+      <li key={index} onClick={() => setInputValue(note)}>
+        {note}
+        <button onClick={(e) => { e.stopPropagation(); removeNote(index); }}>Удалить</button>
+        <button onClick={() => doSmth(index)}>Квадрат</button> 
+      </li>
+    );
+  });
+
+  // Изменение текста в li по потере фокуса инпута
+  function handleBlur() {
+    const index = notes.indexOf(inputValue);
+    if (index !== -1) {
+      let copy = [...notes];
+      copy[index] = inputValue; 
+      setNotes(copy);
     }
-  };
+  }
 
-	const result = notes.map((note, index) => {
-		return <li key={index}>{note}</li>;
-	});
-	
-	return <div>
+  // Перевернуть порядок li
+  function reverseNotes() {
+    setNotes([...notes].reverse());
+  }
+
+  return (
     <div>
-      Задача 1:
-		<ul>
-			{result}
-		</ul>
-    <button onClick={addNote}>Добавить элемент</button>
-    </div>
-    <div>
-      Задача 2: <br />
+      <button onClick={reverseNotes}>Перевернуть список</button>
+      <ul>
+        {result}
+      </ul>
       <input 
-          type="text" 
-          value={inputValue} 
-          onChange={(e) => setInputValue(e.target.value)} 
-          placeholder="Введите текст" 
-        />
-        <button onClick={addNote2}>Добавить заметку</button> 
+        type="text" 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+        onBlur={handleBlur} 
+      />
     </div>
-	</div>;
+  );
 }
 
 export default App;
