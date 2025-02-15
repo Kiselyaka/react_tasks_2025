5567fbf4a79364d63363cd4f030303528a8354a2
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-15 22:24:57 +0300

    React: Задание №75

--- src/App.js
+++ src/App.js
@@ -1,59 +1,80 @@
 import React, { useState } from 'react';
 
+const initNotes = [
+  {
+    id: 'GYi9G_uC4gBF1e2SixDvu',
+    prop1: 'value11',
+    prop2: 'value12',
+    prop3: 'value13',
+  },
+  {
+    id: 'IWSpfBPSV3SXgRF87uO74',
+    prop1: 'value21',
+    prop2: 'value22',
+    prop3: 'value23',
+  },
+  {
+    id: 'JAmjRlfQT8rLTm5tG2m1L',
+    prop1: 'value31',
+    prop2: 'value32',
+    prop3: 'value33',
+  },
+];
+
 function App() {
-  const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const [inputValue, setInputValue] = useState('');
+  const [notes, setNotes] = useState(initNotes);
+  const [newProp1, setNewProp1] = useState('');
+  const [newProp2, setNewProp2] = useState('');
+  const [newProp3, setNewProp3] = useState('');
 
-  function doSmth(index) {
-    let copy = [...notes];
-    copy[index] = copy[index] ** 2; 
-    setNotes(copy);
-  }
+  const addNote = () => {
+    const newNote = {
+      id: Date.now().toString(), 
+      prop1: newProp1,
+      prop2: newProp2,
+      prop3: newProp3,
+    };
+    
+    setNotes([...notes, newNote]); 
+    setNewProp1('');
+    setNewProp2('');
+    setNewProp3('');
+  };
 
-  // Удаление элемента
-  function removeNote(index) {
-    let copy = notes.filter((_, i) => i !== index); 
-    setNotes(copy);
-  }
-
-  // Ввод текста в инпут при клике на li
-  const result = notes.map((note, index) => {
+  const result = notes.map(note => {
     return (
-      <li key={index} onClick={() => setInputValue(note)}>
-        {note}
-        <button onClick={(e) => { e.stopPropagation(); removeNote(index); }}>Удалить</button>
-        <button onClick={() => doSmth(index)}>Квадрат</button> 
+      <li key={note.id}>
+        <span>{note.prop1}</span>
+        <span>{note.prop2}</span>
+        <span>{note.prop3}</span>
       </li>
     );
   });
 
-  // Изменение текста в li по потере фокуса инпута
-  function handleBlur() {
-    const index = notes.indexOf(inputValue);
-    if (index !== -1) {
-      let copy = [...notes];
-      copy[index] = inputValue; 
-      setNotes(copy);
-    }
-  }
-
-  // Перевернуть порядок li
-  function reverseNotes() {
-    setNotes([...notes].reverse());
-  }
-
   return (
     <div>
-      <button onClick={reverseNotes}>Перевернуть список</button>
       <ul>
         {result}
       </ul>
       <input 
         type="text" 
-        value={inputValue} 
-        onChange={(e) => setInputValue(e.target.value)} 
-        onBlur={handleBlur} 
+        value={newProp1} 
+        onChange={(e) => setNewProp1(e.target.value)} 
+        placeholder="Введите prop1" 
       />
+      <input 
+        type="text" 
+        value={newProp2} 
+        onChange={(e) => setNewProp2(e.target.value)} 
+        placeholder="Введите prop2" 
+      />
+      <input 
+        type="text" 
+        value={newProp3} 
+        onChange={(e) => setNewProp3(e.target.value)} 
+        placeholder="Введите prop3" 
+      />
+      <button onClick={addNote}>Добавить элемент</button>
     </div>
   );
 }
