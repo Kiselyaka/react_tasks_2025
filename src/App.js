93e1d25b7b0c5a0a91d4b2c4b1d43b4ef1873e5f
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 23:01:49 +0300

    React - Высший учебник: Задание №8

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState} from 'react';
+import { MyContext } from './MyContext.js';
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState('white');
-
-  const handleClick = () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-    setBackgroundColor(randomColor);
-  };
-
-  useEffect(() => {
-    document.body.style.backgroundColor = backgroundColor;
-
-    return () => {
-      document.body.style.backgroundColor = 'white';
-    };
-  }, [backgroundColor]);
-
   return (
-    <div onClick={handleClick} style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Кликните где угодно, чтобы изменить фон!</h1>
-    </div>
+    <MyContext.Provider value={{ /* Здесь можно указать данные, которые вы хотите передать */ }}>
+      <div>
+        <h1>Пример использования контекста</h1>
+      </div>
+    </MyContext.Provider>
   );
 }
 
