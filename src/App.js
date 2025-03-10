a2a8e4d5b107c7ffba865758988001390e3228b6
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 22:14:33 +0300

    React: Задание №112 Блокнот

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const DIVA = styled.div`
-  width: 150px;
-  height: 150px;
-  background-color: yellow;
-  border: 2px solid black; 
-`;
-
-const DIVB = styled(DIVA)`
-  background-color: green; 
-  border-width: 3px; 
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center; 
-`;
+import './App.css';
+import Notebook from './Notebook';
 
 function App() {
   return (
-    <Container>
-      <DIVA /> {/* Первый див */}
-      <DIVB /> {/* Второй див */}
-      <DIVA /> {/* Третий див */}
-    </Container>
+    <div className="App">
+      <Notebook />
+    </div>
   );
 }
 
