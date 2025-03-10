ebc304d7f0df84afd446d6f4dc80ac9c4b2d9d6d
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 20:15:04 +0300

    React: Задание №99

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
-import './styles.css';
-
 function App() {
+	const class1 = {
+		width: '200px',
+		border: '2px solid brown',
+		padding: '10px',
+		textAlign: 'center',
+	};
+
+	const class2 = {
+		color: 'orangered',
+		fontWeight: 'bold',
+	};
+
+	const class3 = {
+		fontStyle: 'italic',
+		color: 'brown',
+	};
+
+	const class4 = {
+		backgroundColor: 'orange',
+		fontWeight: 'bold',
+		color: 'white',
+	};
+
 	return (
-		<div class="class1">
-	<button class="class3">button1</button>
-	<button class="class4">button2</button>
-</div>	
+		<div style={class1}>
+			<button style={class2}>text</button>
+			<button style={class3}>text</button>
+		</div>
 	);
-}
+} 
 
 export default App;
