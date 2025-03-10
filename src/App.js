de15d0851cfdd4d97aa6e2cefdb5a5c3724015ef
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 20:52:19 +0300

    React: Задание №106

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import styled from "styled-components";
-import Block from "./Block";
+import React from 'react';
+import styled from 'styled-components';
+
+const DIVA = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: yellow;
+  border: 2px solid black; 
+`;
+
+const DIVB = styled(DIVA)`
+  background-color: green; 
+  border-width: 3px; 
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+`;
 
 function App() {
-	return (
-		<Block></Block>
-	);
+  return (
+    <Container>
+      <DIVA /> {/* Первый див */}
+      <DIVB /> {/* Второй див */}
+      <DIVA /> {/* Третий див */}
+    </Container>
+  );
 }
 
 export default App;
