4eba00893fa13956bb80bc5c8d272eb313a368a9
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 21:41:59 +0300

    React: Задание №108_ошибся с ветками

--- test2/inputs/src/components/Inputs.js
+++ test2/inputs/src/components/Inputs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Inputs.module.css';
 
-const Inputs = () => (
-  <>
-    <input type="text" className={styles.input} placeholder="Input 1" />
-    <input type="text" className={styles.input} placeholder="Input 2" />
-    <input type="text" className={styles.input} placeholder="Input 3" />
-  </>
-);
+function Inputs() {
+  return (
+    <div>
+      <input type="text" className={styles.input} placeholder="Input 1" />
+      <input type="text" className={styles.input} placeholder="Input 2" />
+      <input type="text" className={styles.input} placeholder="Input 3" />
+    </div>
+  );
+}
 
 export default Inputs;
