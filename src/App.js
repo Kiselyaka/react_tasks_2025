1422dc131b976c8216c68d0b0be680c62298f72c
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-10 22:44:25 +0300

    React - Высший учебник: Задание №5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useState} from 'react';
 
 function App() {
+  const [username, setUsername] = useState(() => {
+    return localStorage.getItem('username') || '';
+  });
+  
   useEffect(() => {
-    document.title = 'Мой новый заголовок страницы';
-    document.body.style.backgroundColor = 'green';
-  }, []); 
+    localStorage.setItem('username', username);
+  }, [username]); 
+  const changeUsername = (event) => {
+    setUsername(event.target.value); 
+  };
 
   return (
     <div>
       <h1>React App</h1>
+      <input 
+        type="text" 
+        value={username} 
+        onChange={changeUsername} 
+        placeholder="Введите имя пользователя" 
+      />
+      <p>Текущее имя пользователя: {username}</p>
     </div>
   );
 }
