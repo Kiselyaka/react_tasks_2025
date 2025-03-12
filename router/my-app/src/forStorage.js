da5779e2e61c636e66978163642080353ea43da2
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-12 23:37:49 +0300

    React - Высший учебник: Задание №33 - перепутал ветку

--- router/my-app/src/forStorage.js
+++ router/my-app/src/forStorage.js
@@ -1,4 +1,5 @@
 import localforage from 'localforage';
+import { nanoid } from 'nanoid'
 
 export async function getStudents() {
   await someNetwork(); 
@@ -7,11 +8,39 @@ export async function getStudents() {
   return students;
 }
 
+export async function createStudent() {
+    await someNetwork();
+    let id = nanoid(6);
+    let student = { id }; 
+    let students = await getStudents();
+    students.unshift(student); 
+    await setStudents(students); 
+    return student; 
+}
+
+function setStudents(students) {
+    return localforage.setItem('students', students); 
+}
+
 export async function getProducts() {
   await someNetwork(); 
   let products = await localforage.getItem('products');
   if (!products) products = []; 
   return products;
+}
+
+export async function createProduct() {
+	await someNetwork();
+	let id = nanoid(6);
+	let product = { id };
+	let products = await getProducts();
+	products.unshift(product);
+	await setProducts(products);
+	return product;
+}
+
+function setProducts(products) {
+	return localforage.setItem('products', products);
 }
 
 let someCache = {};
